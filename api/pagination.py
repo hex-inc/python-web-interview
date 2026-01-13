86222a05089c8f8085b5abdd6c34462c28449a10
Sukwan Lee <sukwan@hex.tech>
2026-01-13 15:52:31 -0800

    Update comments and don't batch logs

--- api/pagination.py
+++ api/pagination.py
@@ -7,12 +7,11 @@ def get_page(page_size: int, start_after: Optional[Project] = None):
     """
     TODO: Implement this function!
 
-    Given a list of items and pagination parameters, return a Page object containing:
+    Given pagination parameters, return a Page object containing:
     - The correct slice of items based on the pagination parameters
     - A boolean indicating if there are more results available
 
     Parameters:
-        items: List of items to paginate
         page_size: How many items to return per page
         start_after: Optional item to start pagination after
 
@@ -34,13 +33,11 @@ def get_page_filtered(
     """
     TODO: Implement this function!
 
-    Given a list of items, a filter function, and pagination parameters,
-    return a Page object containing:
+    Given pagination parameters and a filter function, return a Page object containing:
     - The correct slice of filtered items based on the pagination parameters
     - A boolean indicating if there are more results available
 
     Parameters:
-        items: List of items to paginate
         page_size: How many items to return per page
         filter_fn: Function to filter items
         start_after: Optional item to start pagination after
